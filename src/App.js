27138166d3dc3a10b92cdfb4a5ecf9612db63877
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-11-06 20:25:32 -0500

    made grid

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Grid />
+      <Grid size={5} />
     </div>
   );
 }
 
 function Voxel(props) {
   return (
-    <button className="voxel">
-      Yay a button
+    <button className="voxel" onClick={() => alert('hi')}>
+      { props.value}
     </button>
   )
 }
@@ -20,14 +20,30 @@ function Voxel(props) {
 class Grid extends React.Component {
   constructor(props) {
     super(props);
-    const size = 10;
     this.state = {
-      voxels: new Array(size).fill(null),
+      voxels: new Array(this.props.size ** 2).fill(null),
     }
   }
 
+  renderVoxel(i) {
+    return (
+      <Voxel value={i} />
+    )
+  }
+
   render() {
-    return <Voxel />
+    const toShow = this.state.voxels.slice();
+    for (var i = 0; i < this.props.size ** 2; i += this.props.size) {
+      var row = new Array(this.props.size);
+      for (var j = 0; j < this.props.size; j++) {
+        row.push(<Voxel value={j} />);
+      }
+      toShow.push(row)
+    }
+    console.log(toShow);
+    return (
+      toShow.map((number) => <div>{number}</div>)
+    );
   }
 }
 
