d563b6ca1a1c4f6b112dc5c5e12608f6e416ca19
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-11-06 19:32:45 -0500

    added grid and voxel components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid />
     </div>
   );
+}
+
+function Voxel(props) {
+  return (
+    <button className="voxel">
+      Yay a button
+    </button>
+  )
+}
+
+class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+    const size = 10;
+    this.state = {
+      voxels: new Array(size).fill(null),
+    }
+  }
+
+  render() {
+    return <Voxel />
+  }
 }
 
 export default App;
